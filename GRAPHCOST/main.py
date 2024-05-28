e1fc86dee622789a46e3978f9471a95685822f62
C <carsten.fischer@gmx.at>
2024-05-28 21:35:05 +0200

    Implemented the Dijkstra-algorithm. But, there are test cases on CodeChef, where the algorithm fails.

--- GRAPHCOST/main.py
+++ GRAPHCOST/main.py
@@ -1,5 +1,36 @@
+import heapq
+
+
+def compute_distance(i, j, a):
+    return abs(i - j) * max(a[i], a[j])
+
+
 def solve_problem(N, a):
-    pass
+    """We use the Dijkstra-algorithm to solve the problem."""
+    # Introduces an array, which stores the information,
+    # which vertices are already visited
+    nodes_visited = [i == 0 for i in range(N)]
+
+    # A priority queue
+    q = []
+
+    # Initialize priority queue with distances from starting node
+    for i in range(N):
+        heapq.heappush(q, (compute_distance(0, i, a), i))
+
+    # While the final node is not visited
+    while not nodes_visited[N-1]:
+        # Select the node with the smallest node from the queue
+        distance_node_current, node_current = heapq.heappop(q)
+        # If selected node is the last node we can stop the search
+        if node_current == N-1:
+            return distance_node_current
+        # Otherwise, we proceed from the new node
+        elif not nodes_visited[node_current]:
+            nodes_visited[node_current] = True
+            for i in range(N):
+                if not nodes_visited[i]:
+                    heapq.heappush(q, (distance_node_current + compute_distance(node_current, i, a), i))
 
 
 if __name__ == "__main__":
--- GRAPHCOST/tests/tests.py
+++ GRAPHCOST/tests/tests.py
@@ -1,10 +1,19 @@
 import unittest
 
-from GRAPHCOST.main import solve_problem
+from GRAPHCOST.main import (
+    compute_distance,
+    solve_problem
+)
 
 
 class TestGRAPHCOST(unittest.TestCase):
     """Test cases for problem GRAPHCOST"""
+
+    def test_compute_distance(self):
+        self.assertEqual(compute_distance(0, 1, [2, 1, 2, 1]), 2)
+        self.assertEqual(compute_distance(1, 0, [2, 1, 2, 1]), 2)
+        self.assertEqual(compute_distance(0, 3, [2, 1, 2, 1]), 6)
+        self.assertEqual(compute_distance(2, 0, [2, 1, 2, 1]), 4)
 
     def test_solve_problem(self):
         self.assertEqual(solve_problem(4, [2, 1, 1, 1]), 4)
