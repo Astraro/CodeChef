0b36bf9f8ffc8946e985f17933c29698b09aa3ab
C <carsten.fischer@gmx.at>
2024-05-29 19:47:18 +0200

    Introduced an array that stores the information which nodes are already visited.

--- GRAPHCOST/main.py
+++ GRAPHCOST/main.py
@@ -9,6 +9,7 @@ def solve_problem(N, a):
     """We use the Dijkstra-algorithm to solve the problem."""
     # Introduces an array that contains the distances from node 0
     distances = [N**3 for _ in range(N)]
+    visited = [False for _ in range(N)]
 
     # A priority queue
     q = []
@@ -17,16 +18,18 @@ def solve_problem(N, a):
     while True:
         # Select the node with the smallest distance from the queue
         distance_node_current, node_current = heapq.heappop(q)
+        visited[node_current] = True
 
         if node_current == N-1:
             return distance_node_current
 
         for neighbour in range(N):
-            distance_current_neighbour = compute_distance(node_current, neighbour, a)
-            distance_start_current_neighbour = distance_node_current + distance_current_neighbour
-            if distance_start_current_neighbour < distances[neighbour]:
-                distances[neighbour] = distance_start_current_neighbour
-                heapq.heappush(q, (distance_start_current_neighbour, neighbour))
+            if not visited[neighbour]:
+                distance_current_neighbour = abs(node_current - neighbour)*max(a[node_current], a[neighbour])
+                distance_start_current_neighbour = distance_node_current + distance_current_neighbour
+                if distance_start_current_neighbour < distances[neighbour]:
+                    distances[neighbour] = distance_start_current_neighbour
+                    heapq.heappush(q, (distance_start_current_neighbour, neighbour))
 
 
 if __name__ == "__main__":
