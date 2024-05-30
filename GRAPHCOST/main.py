94a1612ab2e49000fe57d8fd022eb48463e3ca0f
C <carsten.fischer@gmx.at>
2024-05-30 11:12:06 +0200

    Implemented a new approach, since Dijkstra was too slow. The new approach takes advantage of the properties of the problem. The new algorithm has a linear running time.

--- GRAPHCOST/main.py
+++ GRAPHCOST/main.py
@@ -5,7 +5,7 @@ def compute_distance(i, j, a):
     return abs(i - j) * max(a[i], a[j])
 
 
-def solve_problem(N, a):
+def solve_problem_using_dijkstra(N, a):
     """We use the Dijkstra-algorithm to solve the problem."""
     # Introduces an array that contains the distances from node 0
     distances = [N**3 for _ in range(N)]
@@ -30,6 +30,41 @@ def solve_problem(N, a):
                 if distance_start_current_neighbour < distances[neighbour]:
                     distances[neighbour] = distance_start_current_neighbour
                     heapq.heappush(q, (distance_start_current_neighbour, neighbour))
+
+
+def solve_problem(N, a):
+    """It turned out that Dijkstra was too slow.
+       The following strategy seems to work:
+       - We process the nodes in increasing order starting from node 0.
+         Everytime, we have a node with weight that is at most the weight
+         of the predecessor we choose this node for our path.
+       - Afterwards, we use the same strategy going backwards from node N-1.
+    """
+
+    distance = 0
+    predecessor = 0
+
+    for node_current in range(1, N):
+        if a[node_current] <= a[predecessor]:
+            distance += (node_current - predecessor) * max(a[predecessor], a[node_current])
+            predecessor = node_current
+
+    # In case that we already find a path to the final node, we can return the distance
+    if predecessor == N-1:
+        return distance
+
+    # Otherwise, we use the same strategy going backwards from N-1
+    end_point = predecessor
+    predecessor = N-1
+    node_current = N-2
+    while node_current >= end_point:
+        if a[node_current] <= a[predecessor]:
+            distance += (predecessor - node_current) * max(a[predecessor], a[node_current])
+            predecessor = node_current
+
+        node_current -= 1
+
+    return distance
 
 
 if __name__ == "__main__":
--- GRAPHCOST/tests/tests.py
+++ GRAPHCOST/tests/tests.py
@@ -2,7 +2,8 @@ import unittest
 
 from GRAPHCOST.main import (
     compute_distance,
-    solve_problem
+    solve_problem,
+    solve_problem_using_dijkstra
 )
 
 
@@ -15,7 +16,18 @@ class TestGRAPHCOST(unittest.TestCase):
         self.assertEqual(compute_distance(0, 3, [2, 1, 2, 1]), 6)
         self.assertEqual(compute_distance(2, 0, [2, 1, 2, 1]), 4)
 
+    def test_solve_problem_using_dijkstra(self):
+        self.assertEqual(solve_problem_using_dijkstra(4, [2, 1, 1, 1]), 4)
+        self.assertEqual(solve_problem_using_dijkstra(3, [3, 2, 1]), 5)
+        self.assertEqual(solve_problem_using_dijkstra(5, [2, 4, 5, 2, 3]), 9)
+        self.assertEqual(solve_problem_using_dijkstra(
+            10, [4, 3, 1, 1, 2, 4, 1, 2, 2, 3]), 18
+        )
+        self.assertEqual(solve_problem_using_dijkstra(7, [3, 2, 1, 1, 4, 5, 1]), 9)
+
     def test_solve_problem(self):
         self.assertEqual(solve_problem(4, [2, 1, 1, 1]), 4)
         self.assertEqual(solve_problem(3, [3, 2, 1]), 5)
         self.assertEqual(solve_problem(5, [2, 4, 5, 2, 3]), 9)
+        self.assertEqual(solve_problem(10, [4, 3, 1, 1, 2, 4, 1, 2, 2, 3]), 18)
+        self.assertEqual(solve_problem(7, [3, 2, 1, 1, 4, 5, 1]), 9)
