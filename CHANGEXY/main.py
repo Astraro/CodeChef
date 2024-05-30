23ce2afd54a56c25f5fcd0ba5ef5bc1daf973721
C <carsten.fischer@gmx.at>
2024-05-30 18:24:59 +0200

    Tweaked the approach. But the algorithm fails on a hidden test case on CodeChef.

--- CHANGEXY/main.py
+++ CHANGEXY/main.py
@@ -2,15 +2,24 @@ def solve_problem(a, b, k):
     """First approach to solve the problem: We start from b.
        If it is possible to divide b by k without remainder,
        and the result is still at least as large as a,
-       we divide b by k. Otherwise, we decrease b by 1.
+       we divide b by k. Otherwise, we decrease b several times by 1.
     """
     n = 0
+    i = 0
     while b > a:
-        if (b % k == 0) & (b/k >= a):
-            b /= k
+        remainder = b % k
+        # Case 1: b is divisible by k, and the result is at least as large as a
+        if (remainder == 0) & (b/k >= a):
+            b = b // k
+            n += 1
+        # Case 2: b is divisible by k, but the result is smaller than a
+        elif remainder == 0:
+            n += (b - a)
+            break
+        # Case 3: b is not divisible by k
         else:
-            b -= 1
-        n += 1
+            b -= remainder
+            n += remainder
 
     return n
 
